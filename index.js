2d597d248514ec6564c724103049819b9cd6b610
Lucian648 <68011406+Lucian648@users.noreply.github.com>
2020-12-14 17:11:27 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json')
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+	console.log(`bot logged in as client.user.bot`);
+})
+
+client.on('message', message =>{
+     if (message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])) {
+		 
+	console.log(message.content);
+
+	 if(message.content.startsWith(`${prefix}kick`)) {
+		 //message.channel.send("ok")
+
+		 let member = message.mentions.members.first();
+		 member.kick().then((member) => {
+
+            message.channel.send(":wave: " + member.displayName + " has been kicked. You can now continue your day: :smile:")
+			
+		 })
+		}
+
+	 }
+})
+
+client.login(token);
+
+//To run your bot run this command "node index.js"
